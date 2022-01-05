8f9b0edc16b47352f2b8808b9f4c3ff5cbf8d1b8
Raz <razvan.tirboaca@gmail.com>
2022-01-05 11:15:57 +0100

    feat: initial commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,21 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation")
+
 module.exports = function  (eleventyConfig) {
-    eleventyConfig.addWatchTarget("./src/sass/");
+    eleventyConfig.addPlugin(eleventyNavigationPlugin)
+    eleventyConfig.addWatchTarget("./src/sass/")
+    eleventyConfig.addPassthroughCopy("./src/assets/")
     
     return {
         dir: {
             input: "src",
             output: "public",
+            includes: 'includes',
+            layouts: 'layouts',
+            data: 'data'
         },
-    };
-};
+        templateFormats: [ "md", "njk", "html", ],
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk"
+    }
+}
